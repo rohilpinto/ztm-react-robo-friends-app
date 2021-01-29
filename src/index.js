329e0ec085dcf115afe6c5bb7a315fca47c8c2a5
rohilpinto <rohilcrispinto@gmail.com>
2021-01-29 10:53:30 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
- 
-import {robots} from './robots'
- 
-import CardList from './CardList'
-import reportWebVitals from './reportWebVitals';
-import App from './App'
-import 'tachyons';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
+ 
+ 
+import reportWebVitals from "./reportWebVitals";
+import App from "./containers/App";
+import "tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     {/* <CardList robots={robots}/> */}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
